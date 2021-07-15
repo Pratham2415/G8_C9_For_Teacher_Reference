46f1dde99a510fcafa103c9318fb47e3b5705d57
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-07-15 15:29:16 +0530

    Update Sol_TA_2.py

--- Sol_TA_2.py
+++ Sol_TA_2.py
@@ -1,6 +1,9 @@
 import turtle
 import random
 
+list1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
+colors=["red","green","black","blue","yellow","grey","cyan","orange"]
+
 def draw_thing(l,c):
     for value in l:
         turtle.forward(value)
@@ -9,6 +12,4 @@ def draw_thing(l,c):
         turtle.backward(value)
         turtle.right(30)
 
-list1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
-colors=["red","green","black","blue","yellow","grey","cyan","orange"]
 draw_thing(list1,colors)
